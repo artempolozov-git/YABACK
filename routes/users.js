0d9bf6eb517c0619ed5e83442f58025b6093358e
Artem Polozov <61915288+artempolozov-git@users.noreply.github.com>
2023-10-05 20:56:07 +0300

    Update users.js
    novosat

--- routes/users.js
+++ routes/users.js
@@ -3,51 +3,52 @@ var router = express.Router();
 const axios = require('axios');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('split integration');
+router.get('/', function (req, res, next) {
+    res.send('split integration');
 });
 
-router.post('/split', function(req, res, next) {
-  let data = {
-    orderId: `${Math.random()}`,
-    cart: {
-      items: [
-        {
-          productId: "3",
-          total: req.body.prices,
-          title: req.body.products,
-          quantity: {
-            count: "1",
-          },
+router.post('/split', function (req, res, next) {
+    let card = JSON.parse(req.body.card);
+    let data = {
+        orderId: `${Math.round(Math.random() * 100)}`,
+        cart: {
+            items: card.map((item, index) => {
+                return {
+                    productId: String(index+1),
+                    total: String(item.price),
+                    title: item.name,
+                    quantity: {
+                        count: String(item.colvo),
+                    },
+                }
+            }),
+            total: {
+                amount: req.body.itogo,
+            },
         },
-      ],
-      total: {
-        amount: req.body.prices,
-      },
-    },
-    currencyCode: "RUB",
-    merchantId: "1d2f6b86-21f8-4c25-b928-4d6dcd8a409a",
-    redirectUrls: {
-      onSuccess: "https://the-system.ru/",
-      onError: "https://the-system.ru/404",
-    },
-    availablePaymentMethods: ["SPLIT"],
-  };
-  let config = {
-    headers: {
-      'Authorization': `Api-Key 1d2f6b8621f84c25b9284d6dcd8a409a.wewctQn3ZN1O1CKssc0Hl-fEcqFeO33k`,
-    },
-  };
-  console.log (req);
-  axios.post ('https://pay.yandex.ru/api/merchant/v1/orders',
-      data, config,)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((response) => {
-        //console.log(response);
-        res.status(400).send(response.error);
-      });
-});
+        currencyCode: "RUB",
+        merchantId: "41344f41-3a13-427b-a2f7-fe0fd54c85b7",
+        redirectUrls: {
+            onSuccess: "https://novasat.ru/",
+            onError: "https://novasat.ru/",
+        },
+        availablePaymentMethods: ["SPLIT", "CARD"],
+    };
 
+    let config = {
+        headers: {
+            'Authorization': `Api-Key 41344f413a13427ba2f7fe0fd54c85b7.MSnyviclfF07C62C-4vSzF04T2yRdR6t`,
+        },
+    };
+    //console.log(req);
+    axios.post('https://pay.yandex.ru/api/merchant/v1/orders',
+        data, config)
+        .then((response) => {
+            res.send(response.data);
+        })
+        .catch((response) => {
+            console.log(response);
+            //res.status(400).send(response.error);
+        });
+});
 module.exports = router;
