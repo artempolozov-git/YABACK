db9b5554f79faf57831d3628843004584686e232
Artem Polozov <61915288+artempolozov-git@users.noreply.github.com>
2024-04-03 18:50:55 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,48 +7,46 @@ router.get('/', function (req, res, next) {
     res.send('split integration');
 });
 
-router.post('/split', function (req, res, next) {
-    let card = JSON.parse(req.body.card);
+router.post('/split', function(req, res, next) {
     let data = {
-        orderId: `${Math.round(Math.random() * 100)}`,
+        orderId: `${Math.random()}`,
         cart: {
-            items: card.map((item, index) => {
-                return {
-                    productId: String(index+1),
-                    total: String(item.price),
-                    title: item.name,
+            items: [
+                {
+                    productId: "3",
+                    total: req.body.prices,
+                    title: req.body.products,
                     quantity: {
-                        count: String(item.colvo),
+                        count: "1",
                     },
-                }
-            }),
+                },
+            ],
             total: {
-                amount: req.body.itogo,
+                amount: req.body.prices,
             },
         },
         currencyCode: "RUB",
-        merchantId: "41344f41-3a13-427b-a2f7-fe0fd54c85b7",
+        merchantId: "53907dd4-6dcc-4099-a508-c0e35b3be724",
         redirectUrls: {
-            onSuccess: "https://novasat.ru/",
-            onError: "https://novasat.ru/",
+            onSuccess: "https://idanceballet.ru/sales/shop/dealPaid/id/config/hash/",
+            onError: "https://idanceballet.ru/404",
         },
         availablePaymentMethods: ["SPLIT", "CARD"],
     };
-
     let config = {
         headers: {
-            'Authorization': `Api-Key 41344f413a13427ba2f7fe0fd54c85b7.MSnyviclfF07C62C-4vSzF04T2yRdR6t`,
+            'Authorization': `Api-Key 53907dd46dcc4099a508c0e35b3be724.Unemjk6P3yEIl_EbCZ0McHd9GPfQVk1g`,
         },
     };
-    //console.log(req);
-    axios.post('https://pay.yandex.ru/api/merchant/v1/orders',
-        data, config)
+    //console.log (req);
+    axios.post ('https://pay.yandex.ru/api/merchant/v1/orders',
+        data, config,)
         .then((response) => {
             res.send(response.data);
         })
         .catch((response) => {
-            console.log(response);
-            //res.status(400).send(response.error);
+            //console.log(response);
+            res.status(400).send(response.error);
         });
 });
 module.exports = router;
